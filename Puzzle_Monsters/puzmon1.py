57cd9c99c2bf9deb68f68ecbba66054963122f60
tajima <programming.tajima@gmail.com>
2025-07-24 13:24:58 +0900

    作成開始

---
+++ Puzzle_Monsters/puzmon1.py
@@ -0,0 +1,21 @@
+"""
+作成日:2025/07/24～
+作成者:田島清
+"""
+
+# Puzzle & Monsters
+
+
+# 関数宣言
+def main():
+    # タイトル
+    print("")
+    print("＊＊＊ Puzzle & Monsters ＊＊＊")
+    print("＜ 作成中 ＞")
+
+
+def go_dungeon():
+    
+
+# 実行
+main()
